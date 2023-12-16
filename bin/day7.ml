31fa8b3eae46895564f9b06b4ebc7effb440de7d
Bar <ogoun.d@gmail.com>
2023-12-16 08:33:05 -0500

    Day 7 navigation complete

--- bin/day7.ml
+++ bin/day7.ml
@@ -23,57 +23,67 @@ $ ls
 7214296 k"
 
 type fs_item =
-  | File of string * int * fs_item
-  | Dir of string * fs_item list * fs_item
+  | File of string * int
+  | Dir of string * fs_item list
   | None
 
-type fs_holder = { mutable cur: fs_item; }
-
 let print_fs_item_name itm = match itm with
-  | Dir (name, _, _) | File (name, _, _) -> print_string name
+  | Dir (name, _) | File (name, _) -> print_string name
   | _ -> print_string "-"
 
 let print_fs_item itm = let rec print_fs_item_ind indentation item = print_string indentation; match item with
-  | File (_, size, parent) -> print_string "- "; print_fs_item_name item; print_string " (file, size="; print_int size; print_string ")"; print_fs_item_name parent; print_string "\n"
-  | Dir (_, lst, parent) -> print_string "- "; print_fs_item_name item; print_string " (dir)"; print_fs_item_name parent; print_string "\n"; List.fold_left (fun _ elem -> print_fs_item_ind (indentation ^ "  ") elem) () lst
+  | File (_, size) -> print_string "- "; print_fs_item_name item; print_string " (file, size="; print_int size; print_string ") "; print_string "\n"
+  | Dir (_, lst) -> print_string "- "; print_fs_item_name item; print_string " (dir) "; print_string "\n"; List.fold_left (fun _ elem -> print_fs_item_ind (indentation ^ "  ") elem) () lst
   | None -> () in
   print_fs_item_ind "" itm
 
-let root = Dir ("/", [], None)
+let root = Dir ("/", [])
+
+type fs_holder = { mutable cur: fs_item; }
+type callstack_holder = { mutable cur: fs_item list; }
 
 let fs: fs_holder = { cur = root }
 
-let print_fs () = print_fs_item fs.cur; print_string "\n"
+let callstack: callstack_holder = { cur = [] }
+
+let print_fs title = print_endline title; print_fs_item fs.cur; print_endline ""
 
 let add_item item dir = match dir with
-  | Dir (name, lst, parent) -> (match item with
-    | Dir (child_name, child_lst, _) -> Dir (name, (Dir (child_name, child_lst, dir))::lst, parent)
-    | File (child_name, child_size, _) -> Dir (name, (File (child_name, child_size, dir))::lst, parent)
-    | None -> Dir (name, None::lst, parent))
+  | Dir (name, lst) -> (match item with
+    | Dir (child_name, child_lst) -> Dir (name, (Dir (child_name, child_lst))::lst)
+    | File (child_name, child_size) -> Dir (name, (File (child_name, child_size))::lst)
+    | None -> Dir (name, None::lst))
   | _ -> dir
 
 let rec find_item name lst = match lst with
   | h::t -> (match h with
-    | Dir (h_name, _, _) | File (h_name, _, _) -> if name = h_name then h else find_item name t
+    | Dir (h_name, _) | File (h_name, _) -> if name = h_name then h else find_item name t
     | _ -> None)
   | _ -> None
 
+let update_fs title fn = fs.cur <- fn fs.cur; print_fs (title); Day1.printlist print_fs_item_name callstack.cur; print_endline ""; print_endline ""
+
+let push_callstack node = callstack.cur <- node::callstack.cur
+
+let pop_callstack () = match callstack.cur with
+  | [] -> callstack.cur <- []; None
+  | h::t -> callstack.cur <- t; h
+
 let cd path pwd = match pwd with
-  | Dir (_, lst, parent) -> print_string "cd - \n"; if path = ".." then
-    (print_fs_item parent; parent) else (print_fs_item (find_item path lst); (find_item path lst))
+  | Dir (_, lst) -> if path = ".." then
+    pop_callstack () else (push_callstack pwd; (find_item path lst))
   | _ -> None
 
-let update_fs fn = fs.cur <- fn fs.cur; print_fs ()
 
-let f_file = File ("f", 29116, None)
-let g_file = File ("g", 2557, None)
+let f_file = File ("f", 29116)
+let g_file = File ("g", 2557)
 
-let a_dir = Dir ("a", [], None)
+let a_dir = Dir ("a", [])
 
-let j_file = File ("j", 4060174, None)
-let k_file = File ("k", 7214296, None)
+let j_file = File ("j", 4060174)
+let k_file = File ("k", 7214296)
 
-let d_dir = Dir ("d", [], None)
+let d_dir = Dir ("d", [])
 
 let pack_a = add_item g_file (add_item f_file a_dir)
 
@@ -100,10 +110,12 @@ let pack_d = add_item j_file (add_item k_file d_dir)
 ]) *)
 
 let run () =
-  update_fs (add_item pack_a);
-  update_fs (add_item pack_d);
-  update_fs (cd "a");
-  update_fs (cd "..");
-  update_fs (cd "d");
-  update_fs (cd "..");
+  print_string "\n";
+  print_string "\n";
+  update_fs "Adding A" (add_item pack_a);
+  update_fs "Adding D" (add_item pack_d);
+  update_fs "CD A" (cd "a");
+  update_fs "Go back" (cd "..");
+  update_fs "CD D" (cd "d");
+  update_fs  "Go back" (cd "..");
   print_string "\n";;
