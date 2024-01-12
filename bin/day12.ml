dde421f86e8fe3d3be62c4f042b769624a59c0e6
Bar <ogoun.d@gmail.com>
2024-01-12 14:59:30 -0500

    Day 12 part 1 complete

--- bin/day12.ml
+++ bin/day12.ml
@@ -1,8 +1,44 @@
-let test_string = "Sabqponm
-abcryxxl
-accszExk
-acctuvwj
-abdefghi"
+let test_string = "abccccaaaaaaaaaaaaaccaaaaaaaacccccccccaaaaaaaaccccccccaaacaaacccccccaaaaaaccccccccccccccccccccccaaaacccccccccccacccccccccccccccccccccccccccccccccccccccccccccccaaaa
+abccccaaaaacaaaaaaccccaaaaaaccccccccccaaaaaaacccccccccaaaaaaacccccaaaaaaaaaacccccccccccccccccccaaaaaacccccccccaaaaaaaaccccccccccccccccccccccccccccccccccccccccaaaaa
+abcccaaaaaccaaaaaaccccaaaaaaccccccaacccaaaaaacccccccccaaaaaacccaaaaaaaaaaaaaaacaaccacccccccccccaaaaaaccccccccccaaaaaacccccccccccccccccccccccccccccccccccccccccaaaaa
+abccccccaaccaaaaaaccaaaaaaaaccccccaaacaaaacaaacccccccaaaaaaaaccaaaaaaaaaaaaaaacaaaaacccccccccccccaaccccccccccccaaaaaaccccccccccccccccccccccccccccacccccccccccaaaaaa
+abccccccccccaaccaaccaaaaccaacccccccaaaaaaaccccccccccaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaacccccccaaaaccccccccccccccccaaaaaaaacccccccccccccccccccccccccccaaccccccccccccccaa
+abcccccccaaaaacccaaaaaaaacccaaccccaaaaaaccccccccccccaaaaaaaaaaaaaaaacaaaaaaaccaaaaaaccccccaaaaaccccccccccccccaaaaaaaaaaccaccccccccccccccccccccccccaccccccccccccccca
+abcccccccaaaaacccaaaaaaaaccaaaaccaaaaaaaaccccccccccccccaaacaaaaaaaaacaaaaaacccccaaaacccccaaaaaaccccccccccccccaaaaaaaaaaaaacccccccccccccccllllllccccdccccccccccccccc
+abccccccaaaaaacccccaaaaccccaaaaacaaaaaaaaccccccccccccccaaacccccaaaccccaaaaaacccaaccccccccaaaaaacccccccccccccccccaaaaaaaaaacccccccccccccklllllllllcddddccaccaaaccccc
+abccccccaaaaaacccccaaaaaaaaaaaaaaaccaaccccccaacaacccccccaaccccccccccccaaacaacccccccccccccaaaaaacccccccccccccccccaaaaaaaaaacccccccccccckklllppllllcddddddddaaaaccccc
+abccccccaaaaaaccccaaacaaaaaaaaaaaaccaaccccccaaaaaccccccccccccccccccccccccccccccccccccccccccaaccccccaaccccccccccccaacaaaaaaaccccccccccckklpppppplllmdddddddddacccccc
+abccccccccaaacccccaacccaccaaaaaaccccccccccccaaaaaaccccccccccccccccccccccccccccccccccccccccccccccccaaaaccccccccccccaaaaaaaaaaccccccccckkkkppppppplmmmmmmddddddaacccc
+abccccaaacaaacccccccccccccaaaaaaccccccccccccaaaaaacccccccccccccccccaaaccccccccccccccccccccccccccccaaaaccccccccccccaaaaaaaaaaccccccccckkkppppuppppmmmmmmmmddeeeacccc
+abccccaaaaaaacccccccccccccaaaaaacccaccccccccaaaaaacccccccccccccccccaaaacccccccccccccccccccccaaacccaaaacccccccccccaaaacaaaccccccccccckkkpppuuuuuppqqmmmmmmmmeeeacccc
+abcccccaaaaaaccccccccccccaaaaaaaacaaccccccccccaaaccccccccccccccccccaaaaccccccccccccccccccccaaaaccccccccccccccccccaaaaaaaacccccccccckkkkpppuuuuupqqqqqqqmmmmeeeccccc
+abcccccaaaaaaaacccccccccccaccccaaaaacccccccccccccccccccccccccccccccaaaccccccccccccccaaaccccaaaacccccccccccccaaccaaaaaaaaccccccccckkkkkrrpuuuxuuuqqqqqqqqmmmmeeccccc
+abccccaaaaaaaaaccccccccccccccaaaaaacccccccacaacccccccccccccccccccccccccccccccccccccaaaaaacccaaaccccccccccaaaaccaaaaaaacccccccccckkkkrrrrruuuxxuvvvvvvqqqqnnneeccccc
+abcccaaaaaaaaaaccccccccccccccaaaaaaaacccccaaaaacccccccccccccccaaaaaccccccccccccccccaaaaaaccccccccccccccccaaaaaaaaaaaaacccccccccjjjkrrrrruuuxxxxvvvvvvvqqqnnneeccccc
+abcaaaaacaaacccccccccccccccccaaaaaaaacccccaaaaaccaacccccccccccaaaaaccccccccccccccccaaaaaccccccccccccccccccaaaaaccaaaaaacccccccjjjrrrrruuuuuxxxyvyyyvvvqqqnneeeccccc
+abcaaaaacaaaccaaccccccccccccccccaacccccccaaaaaaaaaaaccccccccccaaaaaaccccccccccccccccaaaaaccccccccccccccccaaaaacccaaaaaaaacaaacjjjrrrtttuuxxxxxyyyyyvvvqqnnneeeccccc
+abaaaaaccaacccaaaccaacccaaccccccaccccccccaaaaaaaaaacccccccccccaaaaaaccccccccccccccccaacaacccccccccccccccccccaacccaaccccaaaaaacjjjrrrtttxxxxxxxyyyyyvvvrrnnneeeccccc
+SbaaaaacccccccaaaaaaaccaaaacccccccccccccccaaaaaaaaacccccccccccaaaaaaccccccccccccccccccccccccccccccccccccccccccccccaacccaaaaaacjjjrrrtttxxxEzzzzyyyvvvrrnnneeecccccc
+abcaaaaacccccccaaaaaaccaaaacccccccccccccccaaaaaaaaacccccccccccccaaccccccccccccccccccccccccccccaaccccccccccaaccccacaaaacaaaaaaajjjrrrtttxxxxxyyyyyvvvrrrnnnfffcccccc
+abcaacccccccaaaaaaaacccaaaaccccccccccccccccaaaaaaaaaaccccccccccccccccccccccccccccccccccccccaaaaaccccccccccaaccccaaaaaaaaaaaaaajjjqqqttttxxxxyyyyyyvvrrrnnnfffcccccc
+abccccccccccaaaaaaaaaccccccccccccccccccccaaaaaaaaaaaaacccccccccccccccccaacccccccccccccccccccaaaaaccccccaacaaaaaccaaaacaaaaaaaacjjjqqqqttttxxyywyyyywvrrnnnfffcccccc
+abccccccccccaaaaaaaaaacccccccccccccccccccaaaaaaaaacaaacccccccccccccaaacaacccccccccccccccccccaaaaaccccccaaaaaaaaccaaaaccccaaacccjjjjqqqqtttxwywwwyywwwrrnnnfffcccccc
+abcccccccccccccaaaaaaacccccccccccccccccccaaaaaaaaaaaaaaaacccccccccccaaaaaccccccccccccccccccaaaaacccaaccccaaaaccccaacaacccaaaccccjjjiqqqtttwwywwwwwwwwrrroofffcccccc
+abcccccccccccccaaaccccccccccccccccccccccaaaaaaaaaaaaaaaaaccccccccccccaaaaaacccccccccccccccccccaaacaaaccccaaaaaccccccccccccccccccciiiiqqqttwwwwwswwwwrrrroofffcccccc
+abcccccccccccccaaccccccccccccaaaacccccccaaaaaaaaccaaaaacccccccccccccaaaaaaacccccccccccccccccccaaaaaaacccaaacaacccccaaaaacccccccccciiiqqqttwwwwsssssrrrrroofffaccccc
+abcccccccccccccccccccccccccccaaaaccccccccacaaacccaaaaaaccccccaaccccaaaaaaccccccccaacaaccccccccaaaaaaccccaaaacacccccaaaaacccccccccciiiqqqtsswsssssssrrrrooofffaccccc
+abcccccccccccccccccccccccccccaaaaccccccccccaaaccaaaaaaaccccccaaaaccaacaaaccccccccaaaaacccccccccaaaaaaaaccaaacacccccaaaaaacccccccccciiqqqssssssspposrrroooofffaccccc
+abccccaaacccccccccccccccccccccaaacccccccccccccccaaacaaaccccaaaaaacccccaaaccccccccaaaaaacccccccaaaaaaaaaaaaaaaaaccccaaaaaaccccccaccciiiqqpsssssppppooooooogffaaccccc
+abccccaaaaaacccaaaccccccccccccccccccccccccccccccccccccaccccaaaaacccccccccccccccccaaaaaaccccccaaaaaaaaaaaaaaaaaaccccaaaaaacccaaaaccciiiqqppppppppppoooooogggfaaacccc
+abcccaaaaaaacccaaaccccccccccccccccccccccccccccccccccccccccccaaaaaccccccccccccccccaaaaaaccccccaaacaaaccccaaaaaacccccccaacccccaaaaaacciiipppppppphgggggggggggaaaacccc
+abccaaaaaaaacccaaacaaacccccccccccccccccccccaacccccccccccccccaacaacccccaacccccccccccaaacccccccccccaaacccccaaaaacccccccccccccccaaaaacciiihppppphhhhgggggggggaaccccccc
+abccaaaaaaacaaaaaaaaaacccccccccccccccccccccaaaccccccacccccccccccccccccaaccccccccccccccccccccccccaaaaccccaaaaaaccccccccccccccaaaaacccciihhhhhhhhhhgggggggccaaccccccc
+abccccaaaaaaaaaaaaaaacccccccccccccccccccaaaaaaaaccccaaacaaaccccccccccaaaaccaaccccccccaacaacccccaaaaaaacccaacccccccccccccccccaacaaccccchhhhhhhhhaaaacccccccccccccccc
+abccccaaaaaacaaaaaaaccccccccccccccccccccaaaaaaaaccccaaaaaaaccccccccccaaaaaaaacaccccccaaaaaccccccaaaaacccccccccccccccccccccccccccccccccchhhhhhacaaaaaccccccccccccccc
+abccccaaccccccaaaaaacccccccccccccaaccccccaaaaaacccccaaaaaaccccccaaaaaaaaaaaaaaaccccccaaaaaacccaaaaaaacccccccccccccccccccccccccccccccccccccaaaaccaaacccccccccccaaaca
+abccccccccccccaaaaaaaccccccccccccaaccccccaaaaaacccaaaaaaaaccccccaaaaaaaaaaaaaacccccccaaaaaacccaaaaaaaaccccccaaacccccccccccccccccccccccccccaaaaccccccccccccccccaaaaa
+abccaaacccccccaaacaaacccccccccaaaaaaaacccaaaaaacccaaaaaaaaacccccaaaaaaaaaaaaaacccccccaaaaaccccaaaaaaaaccccccaaaaccccccccccccccccccccccccccaaaccccccccccccccccccaaaa
+abcaaaacccccccaaccccccccccccccaaaaaaaacccaaccaacccaaaaaaaaaaccccccccaaaaaaacaacccccccccaaaccccccaaacaaccccccaaaacccccccccccccccccccccccccccccccccccccccccccccaaaaaa"
 
 let explode_char str = List.rev (String.fold_left (fun acc elem -> elem::acc) [] str)
 let explode_2d s = List.map explode_char (String.split_on_char '\n' s)
@@ -12,29 +48,29 @@ let twod_array_of_twod_list twod_list =
   let rec loop twod_list i =
     match twod_list with
     | [] -> ()
-    | hd :: tl -> twod_array.(i) <- Array.of_list (List.map (fun x -> int_of_char x - 82) hd); loop tl (i + 1) in
+    | hd :: tl -> twod_array.(i) <- Array.of_list (List.map (fun x -> let use_char = if x = 'S' then 'a' else if x = 'E' then 'z' else x in int_of_char use_char - 97) hd); loop tl (i + 1) in
   loop twod_list 0; twod_array
 
 let get_direction_char diff = match diff with
   | (0, 1) -> '<'
   | (0, -1) -> '>'
   | (1, 0) -> '^'
-  | (-1, 0) -> 'V'
+  | (-1, 0) -> 'v'
   | _ -> ' '
 
 let find_in_path path (i, j) = List.exists (fun (x, y) -> x = i && y = j) path
-
-let get_height heights_array (i, j) = if heights_array.(i).(j) = 1 then 15 else heights_array.(i).(j)
 
 let directions = [(0, 1); (0, -1); (1, 0); (-1, 0)]
 
 let draw_path (height, width) path =
   let map_array = Array.make_matrix height width '.' in
+  let (endi, endj) = List.hd path in
+  map_array.(endi).(endj) <- 'E';
   let rec loop path = match path with
     | [] -> ()
     | (fromi, fromj)::(toi, toj)::tl -> map_array.(fromi).(fromj) <- get_direction_char (toi - fromi, toj - fromj); loop ((toi, toj)::tl)
     | (i, j)::[] -> map_array.(i).(j) <- 'S' in
-  loop path; Array.iter (fun row -> Array.iter (fun elem -> print_char elem) row; print_newline ()) map_array
+  loop (List.tl path); Array.iter (fun row -> Array.iter (fun elem -> print_char elem) row; print_newline ()) map_array
 
 let print_option print_elem opt = match opt with
   | None -> print_string "None"
@@ -46,15 +82,15 @@ let print_int_tuple_list lst = Day1.printlist print_int_tuple lst
 
 let print_int_tuple_list_option lst = print_option print_int_tuple_list lst
 
-let rec find_path height width prev_height checked_array heights_array path (i, j) =
-  if i < 0 || j < 0 || i >= height || j >= width then (None) else (
-  let cell_height = get_height heights_array (i, j) in
-  if (cell_height < 0) then (if prev_height = (int_of_char 'z' - 82) then Some ((i, j)::path) else (None)) else
+let rec find_path height width prev_height checked_array heights_array path (i, j) (endi, endj) =
+  if i < 0 || j < 0 || i >= height || j >= width then (None) else
+  let cell_height = heights_array.(i).(j) in
   if cell_height - prev_height > 1 then (None) else
+  if i = endi && j = endj then Some ((i, j)::path) else
   if (checked_array.(i).(j) <= List.length path + 1) then (None) else (
   checked_array.(i).(j) <- List.length path + 1;
   let rests = List.map (fun (di, dj) ->
-    find_path height width cell_height checked_array heights_array ((i, j)::path) (i + di, j + dj)) directions in
+    find_path height width cell_height checked_array heights_array ((i, j)::path) (i + di, j + dj) (endi, endj)) directions in
   List.fold_left
     (fun min_opt cur_opt ->
     match (min_opt, cur_opt) with
@@ -62,15 +98,16 @@ let rec find_path height width prev_height checked_array heights_array path (i, 
       | (_, Some (_)) -> cur_opt
       | (Some (_), _) -> min_opt
       | _ -> None)
-    None rests))
+    None rests)
 
+let rec find_c cha i lst = match lst with
+  | [] -> -1
+  | hd :: tl -> if hd = cha then i else find_c cha (i + 1) tl
 
-let rec find_start heights_list = let rec find_s i j lst = match lst with
-  | [] -> (-1, -1)
-  | hd :: tl -> if hd = 'S' then (i, j) else find_s i (j + 1) tl in
+let rec find_char cha i heights_list =
   match heights_list with
   | [] -> (-1, -1)
-  | hd :: tl -> let (i, j) = find_s 0 0 hd in if i >= 0 then (i, j) else find_start tl
+  | hd :: tl -> let j = find_c cha 0 hd in if j >= 0 then (i, j) else find_char cha (i + 1) tl
 
 let run () =
   print_newline ();
@@ -82,14 +119,18 @@ let run () =
   let heights_array = twod_array_of_twod_list heights_list in
   print_endline "Heights array:";
   Day11.print_array (Day11.print_array print_int) heights_array;
-  let start_pos = find_start heights_list in
+  let start_pos = find_char 'S' 0 heights_list in
+  let end_pos = find_char 'E' 0 heights_list in
   let (height, width) = (Array.length heights_array, Array.length heights_array.(0)) in
   let checked_array = Array.make_matrix height width max_int in
   print_newline ();
   print_newline ();
   print_endline "Start position:";
   Day5.print_tuple print_int start_pos;
-  let path_opt = find_path height width (int_of_char 'a' - 82) checked_array heights_array [] start_pos in
+  print_newline ();
+  print_endline "End position:";
+  Day5.print_tuple print_int end_pos;
+  let path_opt = find_path height width (int_of_char 'a' - 82) checked_array heights_array [] start_pos end_pos in
   print_newline ();
   print_newline ();
   print_endline "Path:";
