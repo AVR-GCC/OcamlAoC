f1ba7e13f446dc9e68c4226c9e5d8e2491ea6205
Bar <ogoun.d@gmail.com>
2024-02-24 01:46:29 +0700

    My merge sort

--- bin/myutils.ml
+++ bin/myutils.ml
@@ -53,3 +53,19 @@ let fold_lefti f acc lst = let rec fold_lefti' acc lst i = match lst with
   | [] -> acc
   | h::t -> fold_lefti' (f acc h i) t (i + 1) in
   fold_lefti' acc lst 0
+
+let my_merge_sort cmp lst = let rec merge_two_lists lst1 lst2 = match lst1, lst2 with
+  | [], l2 -> l2
+  | l1, [] -> l1
+  | h1::t1, h2::t2 -> let cmp_res = cmp h1 h2 in
+    if cmp_res < 0 then h1::(merge_two_lists t1 lst2)
+    else h2::(merge_two_lists lst1 t2) in
+  let rec do_one_run lst = match lst with
+  | [] -> []
+  | [x] -> [x]
+  | a::b::t -> (merge_two_lists a b)::(do_one_run t) in
+  let rec do_all_runs mlst = match mlst with
+  | [] -> []
+  | [x] -> x
+  | l -> do_all_runs (do_one_run l) in
+  do_all_runs (List.map (fun x -> [x]) lst)
