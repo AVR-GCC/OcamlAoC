8602f39fc0f42d8ca06bc4c5a966375054df359b
Bar <ogoun.d@gmail.com>
2024-05-14 00:35:19 +0700

    Week 17 init

---
+++ bin/day17.ml
@@ -0,0 +1,10 @@
+open Myutils
+
+let lines = read_file "./inputs/day17test.txt"
+
+let run () = print_newline ();
+  print_endline "Day 17";
+  print_newline ();
+  print_newline ();
+  print_endline (List.hd lines);
+  print_newline ();;
