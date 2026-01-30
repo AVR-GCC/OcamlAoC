ff30d7cb77ea5e06e11f4c63abb0ed312c9b61c3
AVR-GCC <ogoun.d@gmail.com>
2026-01-30 21:46:30 +0200

    Day 21 Part 1

--- bin/day21.ml
+++ bin/day21.ml
@@ -55,33 +55,82 @@ let line_to_statement line = line
       Op { id = id; left = Str left; right = Str right; operator = get_op op }
     | _ -> failwith "bad line!"
 
-let rec add_statement map statement =
+let rec add_value values_map operations_map { id; number } =
+  let temp_values_map = StringMap.add id number values_map in
+  match StringMap.find_opt id operations_map with
+    | None -> (temp_values_map, operations_map)
+    | Some { id = oid; left; right; operator } ->
+        match (left, right) with
+        | (Str ls, Str rs) when ls = id ->
+            let new_operation = { id = oid; left = Num number; right = Str rs; operator = operator } in
+            let temp_operations_map = operations_map |> StringMap.remove id |> StringMap.add rs new_operation in
+            (temp_values_map, temp_operations_map)
+        | (Str ls, Str rs) when rs = id ->
+            let new_operation = { id = oid ; left = Str ls; right = Num number; operator = operator } in
+            let temp_operations_map = operations_map |> StringMap.remove id |> StringMap.add ls new_operation in
+            (temp_values_map, temp_operations_map)
+        | (Str ls, Num rn) when ls = id ->
+            let new_value = { id = oid; number = do_op number rn operator } in
+            let temp_operations_map = operations_map |> StringMap.remove id in
+            add_value temp_values_map temp_operations_map new_value
+        | (Num ln, Str rs) when rs = id ->
+            let new_value = { id = oid; number = do_op ln number operator } in
+            let temp_operations_map = operations_map |> StringMap.remove id in
+            add_value temp_values_map temp_operations_map new_value
+        | _ -> failwith "Operation statement with two integers"
+
+let add_operation values_map operations_map { id; left; right; operator } =
+  match (left, right) with
+     | (Str ls, Str rs) -> (
+        match (StringMap.find_opt ls values_map, StringMap.find_opt rs values_map) with
+        | (Some ln, Some rn) ->
+            let number = do_op ln rn operator in
+            add_value values_map operations_map { id = id; number = number }
+        | (Some ln, None) ->
+            let new_operation = { id = id; left = Num ln; right = right; operator = operator } in
+            (values_map, StringMap.add rs new_operation operations_map)
+        | (None, Some rn) ->
+            let new_operation = { id = id; left = left; right = Num rn; operator = operator } in
+            (values_map, StringMap.add ls new_operation operations_map)
+        | (None, None) ->
+            let new_operation = { id = id; left = left; right = right; operator = operator } in
+            (values_map, operations_map |> StringMap.add ls new_operation |> StringMap.add rs new_operation)
+    )
+    | (Str ls, Num rn) -> (
+      match StringMap.find_opt ls values_map with
+      | Some ln ->
+        let number = do_op ln rn operator in
+        add_value values_map operations_map { id = id; number = number }
+      | None -> 
+        let new_operation = { id = id; left = left; right = Num rn; operator = operator } in
+        (values_map, StringMap.add ls new_operation operations_map)
+    )
+    | (Num ln, Str rs) -> (
+      match StringMap.find_opt rs values_map with
+      | Some rn ->
+        let number = do_op ln rn operator in
+        add_value values_map operations_map { id = id; number = number }
+      | None -> 
+        let new_operation = { id = id; left = Num ln; right = right; operator = operator } in
+        (values_map, StringMap.add rs new_operation operations_map)
+    )
+    | _ -> failwith "Operation statement with two integers"
+
+
+let add_statement (values_map, operations_map) statement =
   match statement with
-  | Val { id; number } -> (
-      match StringMap.find_opt id map with
-      | None -> StringMap.add id (Val { id; number }) map
-      | Some Op { id = rec_id; left; right; operator } -> (
-          match (left, right) with
-          | (Str ls, Str rs) when ls = id -> StringMap.add id (Op { id = rec_id; left = Num number; right = Str rs; operator = operator }) map
-          | (Str ls, Str rs) when rs = id -> StringMap.add id (Op { id = rec_id; left = Str ls; right = Num number; operator = operator }) map
-          | (Str ls, Num rs) when ls = id -> add_statement map (Val { id = rec_id; number = do_op number rs operator })
-          | (Num ls, Str rs) when rs = id -> add_statement map (Val { id = rec_id; number = do_op ls number operator })
-          | _ -> failwith "Operation statement with two integers")
-      | _ -> failwith "Found double value statement")
-  | Op { left; right; _ } -> (
-      match (left, right) with
-      | (Str ls, Str rs) -> map |> StringMap.add ls statement |> StringMap.add rs statement
-      | _ -> failwith "start operation statement with an integer")
+  | Val value -> add_value values_map operations_map value
+  | Op operation -> add_operation values_map operations_map operation
 
 let run () = print_newline ();
   print_endline "Day 20";
   print_newline ();
-  let lines = read_file "./inputs/day21.test.txt" in
+  let lines = read_file "./inputs/day21.real.txt" in
   let statements = List.map line_to_statement lines in
   printlist print_statement statements;
   print_newline ();
-  let final_map = List.fold_left add_statement StringMap.empty statements in
-  print_string_map print_statement final_map;
-  (* let root = StringMap.find "root" final_map in *)
-  (* print_statement root; *)
+  let (values_map, _) = List.fold_left add_statement (StringMap.empty, StringMap.empty) statements in
+  (* print_string_map (fun x -> print_int x; print_newline ()) values_map; *)
+  let root = StringMap.find "root" values_map in
+  print_int root;
   print_newline ();;
