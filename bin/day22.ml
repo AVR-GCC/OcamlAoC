13ce8d74c15c6090a2ba6d640ec5a445c1d19fd2
AVR-GCC <ogoun.d@gmail.com>
2026-02-06 17:17:35 +0200

    Use new system for part 1

--- bin/day22.ml
+++ bin/day22.ml
@@ -1,5 +1,6 @@
 open Myutils
 
+(* Types *)
 type orientation = North | East | South | West
 
 let flip = function
@@ -7,10 +8,6 @@ let flip = function
   | East -> West
   | South -> North
   | West -> East
-
-let inc num = function
-  | North | West -> num - 1
-  | _ -> num + 1
 
 let print_orientation = function
   | North -> print_string "North"
@@ -26,29 +23,62 @@ let print_direction = function
 
 type instruction = Dist of int | Turn of direction
 
-type state = {
-  orientation: orientation;
-  position: int * int;
-}
-
-module StateMap = Map.Make(struct
-  type t = state
+module OrientationMap = Map.Make(struct
+  type t = orientation
   let compare = compare
 end)
 
-let ms ori x y = {
-  orientation = ori;
-  position = (x, y)
+type point = {
+  mutable neighbors: point OrientationMap.t;
+  position: int * int;
 }
 
-let print_state { orientation; position } =
+type point_opt = Point of point | Block | Space
+
+type state = {
+  orientation: orientation;
+  point: point;
+}
+
+(* Print *)
+let print_point_pos { position; _ } = print_tuple print_int position
+
+let print_point { neighbors; position } =
+  print_tuple print_int position;
+  print_string " - [";
+  match OrientationMap.find_opt North neighbors with
+  | Some { position; _ } -> print_string " N: "; print_tuple print_int position;
+  | _ -> ();
+  match OrientationMap.find_opt South neighbors with
+  | Some { position; _ } -> print_string " S: "; print_tuple print_int position;
+  | _ -> ();
+  match OrientationMap.find_opt East neighbors with
+  | Some { position; _ } -> print_string " E: "; print_tuple print_int position;
+  | _ -> ();
+  match OrientationMap.find_opt West neighbors with
+  | Some { position; _ } -> print_string " W: "; print_tuple print_int position;
+  | _ -> ();
+  print_string "]"
+
+let print_point_opt = function
+  | Block -> print_string "Block"
+  | Space -> print_string "Space"
+  | Point p -> print_point p
+
+let print_state { orientation; point } =
   print_orientation orientation;
   print_string " ";
-  print_tuple print_int position
+  print_point point
 
 let print_instruction = function
   | Dist x -> print_int x
   | Turn t -> print_direction t
+
+(* Utility *)
+let get_neighbor point orientation =
+  match OrientationMap.find_opt orientation point.neighbors with
+  | None -> point
+  | Some p -> p
 
 let orientation_numbers = function
   | North -> 3
@@ -72,7 +102,98 @@ let turn orientation = function
   | Right -> right_turn orientation
   | Left -> left_turn orientation
 
-let rec walk field ({ orientation; position = (x, y) } as state) instructions =
+let rec transpose mat = function
+  | 0 -> []
+  | i ->
+      let (col, tail) = List.split @@ List.map (fun row ->
+        match row with
+        | h :: t -> (h, t)
+        | _ -> failwith "row not equalized"
+      ) mat in
+      col :: transpose tail (i - 1)
+
+let rec pad_spaces size lst = match (lst, size) with
+  | (_, 0) -> []
+  | ([], x) -> " " :: pad_spaces (x - 1) []
+  | (e :: rest, x) -> e :: pad_spaces (x - 1) rest
+
+let rec get_start_point = function
+  | Point p :: _ -> p
+  | _ :: rest -> get_start_point rest
+  | _ -> failwith "start not found"
+
+(* Initialize *)
+let rec initialize_row row x y =
+  match row with
+  | "." :: rest ->
+      let point = Point { neighbors = OrientationMap.empty; position = (x, y) } in
+      point :: initialize_row rest (x + 1) y
+  | "#" :: rest -> Block :: initialize_row rest (x + 1) y
+  | " " :: rest -> Space :: initialize_row rest (x + 1) y
+  | _ -> []
+
+let initialize_mat mat = List.mapi (fun y row -> initialize_row row 0 y) mat
+
+let get_edges row =
+  let rec get_row_edges_rec start cur =
+    match (start, cur) with
+    | (Space, Space :: t) -> get_row_edges_rec Space t
+    | (Space, h :: t) -> get_row_edges_rec h t
+    | (Space, []) -> failwith "empty row"
+    | (f, [l]) | (f, l :: Space :: _) -> (f, l)
+    | (f, _ :: Block :: t) | (f, _ :: Point _ :: t) -> get_row_edges_rec f t
+    | _ -> failwith "malformed line" in
+  get_row_edges_rec Space row
+
+(* Connect *)
+let connect_points_vertical ({ neighbors = ln; _ } as l) ({ neighbors = rn; _ } as r) =
+    l.neighbors <- OrientationMap.add South r ln;
+    r.neighbors <- OrientationMap.add North l rn
+
+let connect_points_horizontal ({ neighbors = ln; _ } as l) ({ neighbors = rn; _ } as r) =
+    l.neighbors <- OrientationMap.add East r ln;
+    r.neighbors <- OrientationMap.add West l rn
+
+let rec connect_row = function
+  | Point l :: Point r :: rest -> (
+    connect_points_horizontal l r;
+    connect_row (Point r :: rest)
+  )
+  | [] | [_] -> ()
+  | _ :: t -> connect_row t
+
+let rec connect_rows = function
+  | [] -> ()
+  | h :: t -> connect_row h; connect_rows t
+
+let rec connect_col = function
+  | Point l :: Point r :: rest -> (
+    connect_points_vertical l r;
+    connect_col (Point r :: rest)
+  )
+  | _ :: t -> connect_col t
+  | [] -> ()
+
+let rec connect_cols = function
+  | [] -> ()
+  | h :: t -> connect_col h; connect_cols t
+
+let connect_ends_row row =
+  match get_edges row with
+  | (Point ps, Point pf) -> connect_points_horizontal pf ps
+  | _ -> ()
+
+let connect_row_ends mat = List.iter connect_ends_row mat
+
+let connect_ends_col col =
+  match get_edges col with
+  | (Point ps, Point pf) -> connect_points_vertical pf ps
+  | _ -> ()
+
+let connect_col_ends mat = List.iter connect_ends_col mat
+
+(* Walk *)
+let rec walk ({ orientation; point } as state) instructions =
   (* print_state state; *)
   (* print_newline (); *)
   match instructions with
@@ -81,69 +202,35 @@ let rec walk field ({ orientation; position = (x, y) } as state) instructions =
       (* print_instruction ins; *)
       (* print_newline (); *)
       match ins with
-      | Turn d ->
-          walk field { orientation = (turn orientation d); position = (x, y) } rest
+      | Turn d -> walk { orientation = (turn orientation d); point } rest
       | Dist d ->
-          if d = 0 then walk field state rest else
-          let next_state = StateMap.find state field in
-          walk field next_state ((Dist (d - 1)) :: rest)
+          if d = 0 then (walk state rest) else (
+            (* print_point_pos point; print_orientation orientation; print_newline (); *)
+          let next_point = get_neighbor point orientation in
+          let next_state = { point = next_point; orientation } in
+          walk next_state ((Dist (d - 1)) :: rest))
 
-let rec map_row row index row_index start_blocked start_index ori =
-  let next_ind = inc index ori in
-  let path_to_self = (ms ori index row_index, ms ori index row_index) in
-  (* printlist print_string row; *)
-  (* print_string @@ " " ^ string_of_int index; *)
-  (* print_newline (); *)
-  match (row, start_blocked, start_index) with
-  | (" " :: " " :: rest, _, _) -> map_row (" " :: rest) next_ind row_index false None ori
-  | (" " :: "." :: rest, _, _) -> map_row ("." :: rest) next_ind row_index false (Some next_ind) ori
-  | (" " :: "#" :: rest, _, _) -> map_row ("#" :: rest) next_ind row_index true (Some next_ind) ori
-  | ("." :: " " :: _, true, _) -> path_to_self :: []
-  | ("." :: " " :: _, false, Some num) ->
-      let path_to_start = (ms ori index row_index, ms ori num row_index) in
-      let path_from_start = (ms (flip ori) num row_index, ms (flip ori) index row_index) in
-      path_to_start :: path_from_start :: []
-  | ("." :: "." :: rest, _, _) ->
-      let path_to_next = (ms ori index row_index, ms ori next_ind row_index) in
-      let path_from_next = (ms (flip ori) next_ind row_index, ms (flip ori) index row_index) in
-      let rec_call = map_row ("." :: rest) next_ind row_index start_blocked start_index ori in
-      path_to_next :: path_from_next :: rec_call
-  | ("." :: "#" :: rest, _, _) ->
-      let rec_call = map_row ("#" :: rest) next_ind row_index start_blocked start_index ori in
-      path_to_self :: rec_call
-  | ("#" :: " " :: _, true, _) | ([" "], _, _) | ([], _, _) -> []
-  | ("#" :: " " :: _, false, Some num) ->
-      let path_from_start_to_self = (ms (flip ori) num row_index, ms (flip ori) num row_index) in
-      path_from_start_to_self :: []
-  | ("#" :: "." :: rest, _, _) ->
-      let path_from_next_to_self = (ms (flip ori) next_ind row_index, ms (flip ori) next_ind row_index) in
-      let rec_call = map_row ("." :: rest) next_ind row_index start_blocked start_index ori in
-      path_from_next_to_self :: rec_call
-  | ("#" :: "#" :: rest, _, _) -> map_row ("#" :: rest) next_ind row_index start_blocked start_index ori
-  | _ -> failwith "Unexpected details"
-
-let map_col row index row_index start_blocked start_index ori =
-  let mapped_as_row = map_row row index row_index start_blocked start_index ori in
-  List.map (function { orientation; position = (sx, sy) }, { orientation = _ori; position = (cx, cy) } -> ({ orientation; position = (sy, sx) }, { orientation; position = (cy, cx) })) mapped_as_row
-
-let rec pad_spaces size lst = match (lst, size) with
-  | (_, 0) -> []
-  | ([], x) -> " " :: pad_spaces (x - 1) []
-  | (e :: rest, x) -> e :: pad_spaces (x - 1) rest
-
-let init_spaces len = List.init len (function _ -> " ")
-
-let state_to_password { orientation; position = (x, y) } = orientation_numbers orientation + (1000 * (y + 1)) + (4 * (x + 1))
+let state_to_password { orientation; point = { position = (x, y); _ } } = orientation_numbers orientation + (1000 * (y + 1)) + (4 * (x + 1))
 
 let run () = print_newline ();
   print_endline "Day 22";
   print_newline ();
   let lines = read_file "./inputs/day22.real.txt" in
   let reversed = List.rev lines in
-  let instructions_strs = List.hd reversed in
   let map_strs = reversed |> List.tl |> List.tl |> List.rev in
-  (* print_endline "instructions"; *)
-  (* print_newline (); *)
+  let exploded = List.map explode map_strs in
+  let longest_row = max_list @@ List.map List.length exploded in
+  let equalized = List.map (pad_spaces longest_row) exploded in
+  let initialized = initialize_mat equalized in
+  let transposed = transpose initialized longest_row in
+  connect_rows initialized;
+  connect_cols transposed;
+  connect_row_ends initialized;
+  connect_col_ends transposed;
+  (* printlist (fun l -> printlist print_point_opt l; print_newline ()) initialized; *)
+  let start_point = get_start_point @@ List.hd initialized in
+  let start_state = { orientation = East; point = start_point } in
+  let instructions_strs = List.hd reversed in
   let splitted_right = split_on_string "R" instructions_strs in
   let splitted = List.map (split_on_string "L") splitted_right in
   let instructions = List.tl @@ List.fold_left (fun acc left_splitted ->
@@ -152,40 +239,16 @@ let run () = print_newline ();
     let extension = Turn Right :: left_instructions in
     List.append acc extension
   ) [] splitted in
+  (* print_endline "instructions"; *)
+  (* print_newline (); *)
   (* printlist print_instruction instructions; *)
   (* print_newline (); *)
-  let exploded = List.map explode map_strs in
-  let start_x_opt = List.find_index (fun x -> x = ".") @@ List.hd exploded in
-  let start_x = match start_x_opt with
-  | None -> 0
-  | Some x -> x in
-  (* print_endline "map"; *)
-  let longest_row = max_list @@ List.map List.length exploded in
-  let equalized = List.map (pad_spaces longest_row) exploded in
-  (* printlist (fun x -> printlist print_string x; print_newline ()) equalized; *)
-  (* print_newline (); *)
-  let pre = init_spaces (longest_row + 2) in
-  let middle = List.map (function x -> " " :: (x @ [" "])) equalized in
-  let post = init_spaces (longest_row + 2) in
-  let padded = pre :: (middle @ [post]) in
-  (* let lst = List.map (fun r -> List.nth r 4) padded in *)
-  (* let mappings = map_col lst (-1) 3 false None South in *)
-  (* printlist print_string lst; *)
-  (* print_newline (); *)
-  (* printlist (print_mixed_tuple print_state (print_tuple print_int)) mappings; *)
-  (* print_newline (); *)
-  (* print_newline (); *)
-  let horizontal_mappings = List.flatten @@ List.mapi (fun index x -> map_row x (-1) (index - 1) false None East) padded in
-  let misc = init_spaces (longest_row + 2) in
-  let vertical_mappings = List.flatten @@ List.mapi (fun index _ -> map_col (List.map (fun r -> List.nth r index) padded) (-1) (index - 1) false None South) misc in
-  let all_mappings = horizontal_mappings @ vertical_mappings in
-  (* printlist (print_mixed_tuple print_state (print_tuple print_int)) all_mappings; *)
-  (* print_newline (); *)
-  let field = StateMap.of_list all_mappings in
-  let final_state = walk field { orientation = East; position = (start_x, 0) } instructions in
-  print_string "final state: ";
-  print_state final_state;
+  let finish_state = walk start_state instructions in
   print_newline ();
-  print_string "final number: ";
-  print_int @@ state_to_password final_state;
+  print_endline "final state:";
+  print_state finish_state;
+  print_newline ();
+  let password = state_to_password finish_state in
+  print_endline "password:";
+  print_int password;
   print_newline ();;
