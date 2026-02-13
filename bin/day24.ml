cb8a2471d8c43ae97b4175581d5e6b3464fa2bd7
AVR-GCC <ogoun.d@gmail.com>
2026-02-13 14:42:57 +0200

    Day 24 - test passed, real hangs

--- bin/day24.ml
+++ bin/day24.ml
@@ -85,7 +85,7 @@ let move_blizzards map =
   map_blizzards nlst
 
 (* Expidition *)
-let moves = [Some South; Some East; Some West; Some North; None]
+let moves = [Some East; Some South; Some West; Some North; None]
 
 let move_possible bliz point = function
   | None -> if TupleMap.mem point.position bliz then None else Some point
@@ -94,31 +94,49 @@ let move_possible bliz point = function
       if TupleMap.mem (nx, ny) bliz then None else
       let (cx, cy) = point.position in
       if cx = nx && cy = ny then None else
+      if nx = 1 && ny = 0 then None else
       match ori with
-      | North -> if cy > ny then None else Some np
-      | South -> if cy < ny then None else Some np
-      | West -> if cx > nx then None else Some np
-      | East -> if cx < nx then None else Some np
+      | North -> if cy < ny then None else Some np
+      | South -> if cy > ny then None else Some np
+      | West -> if cx < nx then None else Some np
+      | East -> if cx > nx then None else Some np
 
-let rec do_walk bliz height exp =
+let rec do_walk bliz height width points countdown exp =
   let { position = (x, y); _ } = exp in
-  if y = height then 0 else
+  (* print_tuple print_int (height - 1, y); *)
+  (* print_newline (); *)
+  (* print_tuple print_int (x, y); *)
+  (* print_string " -> "; *)
+  (* print_newline (); *)
+  (* print_field bliz (x, y) points; *)
+  if countdown < width - x + height - y - 2 then (max_int, []) else
+  (* if countdown = 0 then (print_endline "coundown finished"; (max_int, [(x, y)])) else *)
+  let next_countdown = countdown - 1 in
+  if y = height - 1 then (0, [(x, y)]) else
+  (* if y = height - 1 then (print_endline "SUCCESS!"; (0, [(x, y)])) else *)
   let coming_bliz = move_blizzards bliz in
   let move_options = List.map (move_possible coming_bliz exp) moves in
   let next_points = List.filter_map Fun.id move_options in
-  print_tuple print_int (x, y);
-  print_string " -> ";
-  printlist print_point_pos next_points;
-  print_newline ();
-  let walks = List.map (do_walk coming_bliz height) next_points in
-  let minutes = 1 + min_list walks in
-  print_string ": ";
-  print_int minutes;
-  minutes
+  if List.length next_points = 0 then (max_int, []) else (
+  (* if List.length next_points = 0 then (print_endline "route dead"; (max_int, [(x, y)])) else ( *)
+  (* printlist print_point_pos next_points; *)
+  (* print_newline (); *)
+  let (minutes, path) = List.fold_left (fun (shortest, path) point -> 
+    let (this_time, this_path) = do_walk coming_bliz height width points shortest point in
+    if this_time < shortest then (this_time, this_path) else (shortest, path)
+  ) (next_countdown, []) next_points in
+  let r_path = (x, y) :: path in
+  let r_mins = minutes + 1 in
+  (* printlist (print_tuple print_int) r_path; *)
+  (* print_string " ->>> "; *)
+  (* print_int r_mins; *)
+  (* print_newline (); *)
+  (* print_newline (); *)
+  (r_mins, r_path))
 
 let run () = print_newline ();
   print_endline "Day 24";
-  let lines = read_file "./inputs/day24.test.txt" in
+  let lines = read_file "./inputs/day24.real.txt" in
   let exploded = List.map explode lines in
   (* print_newline (); *)
   (* printlist (fun x -> printlist print_string x; print_newline ()) exploded; *)
@@ -130,7 +148,10 @@ let run () = print_newline ();
   match exp_opt with
   | Block | Space -> failwith "Bad start position"
   | Point exp -> (
-    let minutes = do_walk blizzards height exp in
+    let s1 = Sys.time () in
+    let (minutes, _) = do_walk blizzards height width points max_int exp in
+    let s2 = Sys.time () in
+    print_endline ("time:" ^ string_of_float (s2 -. s1));
     print_int minutes;
   );
   print_newline ();
